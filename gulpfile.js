88abae4977990448047a7b42c807c49298c43d68
Stewart <stewartwadehines@gmail.com>
2015-10-11 15:27:21 -0500

    Fixed the SASS no-reload problem

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+var prefix = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
 
-// gulp.task('styles', function() {
-//     return gulp.src('./app/sass/*.sass')
-//         .pipe(sass())
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(autoprefixer('last 4 versions'))
-//         .pipe(gulp.dest('./app/css/'));
-//
-// });
 
+// Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
         server: "./app"
     });
 
-    gulp.watch("./app/sass/*.sass", ['sass']);
-    gulp.watch("./app/*.html").on('change', browserSync.reload);
+    gulp.watch("app/sass/*.sass", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("./app/scss/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("./app/css"))
-        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
-            cascade: true
-        }))
+    return gulp.src("app/sass/*.sass")
+        .pipe(sass())
+        .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
-
 
 gulp.task('default', ['serve']);
