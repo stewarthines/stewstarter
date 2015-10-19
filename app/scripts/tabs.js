05049597faf90eb8080fc812a10617dbca90282c
Stewart <stewartwadehines@gmail.com>
2015-10-18 21:26:57 -0500

    added sketch file. played with adding tabs.

---
+++ app/scripts/tabs.js
@@ -0,0 +1,14 @@
+;(function(window) {
+
+    angular.module('app', [])
+        .directive('tab', function() {
+            return {
+                restrict: 'E',
+                transclude: true,
+                template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
+                scope: {},
+                link: function(scope, elem, attr) {}
+            };
+        });
+        
+})(window);
